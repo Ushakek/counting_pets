052655174f04453a75ac81df2ed8dab06b24c7fa
Ушаков Никита Сергеевич <ushakek@yandex.ru>
2021-11-11 22:49:57 +0500

    Update serializer model for pets

--- pets/serializers.py
+++ pets/serializers.py
@@ -8,11 +8,11 @@ class PetsSerializer(serializers.Serializer):
     Повторяет поля класса AllPets, нужен для преобразования данных
     python в json
     """
-    id = serializers.CharField(default=None)
+    id = serializers.ReadOnlyField()
     name = serializers.CharField(max_length=50)
     age = serializers.CharField()
     type = serializers.CharField()
-    # photos = serializers.ReadOnlyField()
+    photos = serializers.ReadOnlyField()
     created_at = serializers.ReadOnlyField()
 
     def create(self, validated_data):
