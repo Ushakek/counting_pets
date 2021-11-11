baa3ae2b1cdcea5dd28895bdd45e0136b47756c4
Ушаков Никита Сергеевич <ushakek@yandex.ru>
2021-11-12 02:50:20 +0500

    Update after testing docker-compose
    - Образ докер собрался после внесения изменений
    Сил нет проверять сегодня на запуск

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,14 @@
 # Установка системы и python
 FROM python:3.8-buster
+
+# Выбор рабочей директории
+WORKDIR /usr/src/app
+
 # Обновление Ubuntu & Обновление pip и setuptools
 RUN apt-get -y update \
+    && apt-get install -y \
+		python3-dev \
+		libpq-dev\
     && apt-get -y clean
 
 # Установка зависимостей
--- countiing_pets/settings.py
+++ countiing_pets/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-w3sb%#!+#_0!#gkai0^x97i9$-4gdlstl5m(@wh&pk9a)3i4o+'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get('DEBUG', default=1))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
