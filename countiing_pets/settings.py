989a2ec3ebb25649b0fdc37de95cf8a4908fc260
Ушаков Никита Сергеевич <ushakek@yandex.ru>
2021-11-11 22:50:06 +0500

    Add media root

--- countiing_pets/settings.py
+++ countiing_pets/settings.py
@@ -126,6 +126,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'images/%Y-%m-%d/')
+MEDIA_URL = '/images/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
